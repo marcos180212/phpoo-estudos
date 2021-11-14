3501fcab17f29f359c98a42ba29cc44d267f26cc
Marcos Antonio <marcosoft.15@gmail.com>
2021-11-14 14:05:38 -0300

    Sobrecarga de propriedades usando os métodos mágicos __get, __set, __isset e __unset

--- SobrecargaDePropriedades.php
+++ SobrecargaDePropriedades.php
@@ -2,36 +2,51 @@
 
 class SobrecargaDePropriedades
 {
+	private $data = [];
 	private $firstname;
 	private $lastname;
 
 	public function __set($name, $value)
 	{
 		if (isset($name) && $name === 'firstname') {
-			$this->firstname = $value;
+			$this->data['firstname'] = $value;
 		}
 
 		if (isset($name) && $name === 'lastname') {
-			$this->lastname = $value;
+			$this->data['lastname'] = $value;
 		}
 	}
 
 	public function __get($name)
 	{
 		if (isset($name) && $name === 'firstname') {
-			return $this->firstname;
+			return $this->data['firstname'];
 		}
 
 		if (isset($name) && $name === 'lastname') {
-			return $this->lastname;
+			return $this->data['lastname'];
 		}
+	}
+
+	public function __isset($name)
+	{
+		return isset($this->data[$name]);
+	}
+
+	public function __unset($name)
+	{
+		echo "Apagando $name\n";
+		unset($this->data[$name]);
 	}
 }
 
 $sp = new SobrecargaDePropriedades();
-$sp->firstname = 'Marcos';
-$sp->lastname = 'Silva';
+$sp->firstname = 'Marcos'; // __set
+$sp->lastname = 'Silva'; // __set
 
-echo "$sp->firstname\n";
-echo "$sp->lastname\n";
+echo "$sp->firstname\n"; // __get
+echo "$sp->lastname\n"; // __get
 var_dump($sp);
+
+var_dump(isset($sp->firstname)); // __isset
+unset($sp->lastname); // __unset
